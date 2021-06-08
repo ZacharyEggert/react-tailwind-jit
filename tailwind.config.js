06dc63926c76772794548c85f79d7da3abd9d69f
Zachary Eggert <Eggert.Zachary@gmail.com>
2021-06-07 21:24:43 -0700

    Boilerplate

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode:'jit',
+  purge: [
+    './src/**/*.{js,jsx,css,html}',
+    './public/**/*.{js,jsx,css,html}'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
